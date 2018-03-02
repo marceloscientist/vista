afec0b93807e57a1aacc0f1912211809bbbf68f6
Marcelo <marcelo.scientist@hotmail.com>
2018-03-02 07:16:08 -0300

    toggle for loading pics sm/xs

--- js/loadingPics.js
+++ js/loadingPics.js
@@ -1,11 +1,27 @@
-function carregarMais(_id) {
+/*** 
+ * 
+ * Only for small devices!
+ * 
+ * ***/
+
+var loaded = false;
+
+function toggle(_id, btn) {
     var slideComponent = document.getElementById(_id);
     var componentsArray = [].slice.call(slideComponent.children);
     slideComponent.innerHTML = "";
-    for (var i = 0; componentsArray.length > 1; i++) {
-        slideComponent.innerHTML += "<article class=\"col-lg-4 col-md-6 col-xs-12\">" +
-            componentsArray[i].innerHTML +
-            "</article>";
+
+    if (!loaded) {
+        loaded = true;
+        for (var i = 0; componentsArray.length > 1; i++) {
+            slideComponent.innerHTML += "<article class=\"col-lg-4 col-md-6 col-xs-12\">" +
+                componentsArray[i].innerHTML + "</article>";
+        }
+    } else {
+        loaded = false;
+        for (var i = 0; i < 2; i++) {
+            slideComponent.innerHTML += "<article class=\"col-lg-4 col-md-6 col-xs-12\">" +
+                componentsArray[i].innerHTML + "</article>";
+        }        
     }
-    document.getElementById("carregar-mais").className = "d-none";
 }
